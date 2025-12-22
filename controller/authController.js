5ff99e6e6c8ad781ac44120128ffb1e1c4303c3c
Feranmi Ayoola <bloggerlucky43@gmail.com>
2025-12-22 19:14:33 +0100

    Same site CORS added

--- controller/authController.js
+++ controller/authController.js
@@ -137,6 +137,7 @@ export const Login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 3600000 * 24,
+      sameSite: "none",
     });
 
     res.json({
